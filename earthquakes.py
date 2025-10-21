18b5dbf469ad20a14bd8c95285cbd8b85ce30a38
fanjingwen53-pixel <fanjingwen53@gmail.com>
2025-10-21 21:55:21 +0100

    improve the code

--- earthquakes.py
+++ earthquakes.py
@@ -1,15 +1,10 @@
-# The Python standard library includes some functionality for communicating
-# over the Internet.
-# However, we will use a more powerful and simpler library called requests.
-# This is external library that you may need to install first.
 import requests
-
+import json   # 导入 json 模块，用来解析数据
 
 def get_data():
-    # With requests, we can ask the web service for the data.
-    # Can you understand the parameters we are passing here?
+    # 从 USGS 服务器请求地震数据
     response = requests.get(
-        "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+        "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
         params={
             'starttime': "2000-01-01",
             "maxlatitude": "58.723",
@@ -18,44 +13,57 @@ def get_data():
             "minlongitude": "-9.756",
             "minmagnitude": "1",
             "endtime": "2018-10-11",
-            "orderby": "time-asc"}
+            "orderby": "time-asc"
+        }
     )
 
-    # The response we get back is an object with several fields.
-    # The actual contents we care about are in its text field:
+    # 获取返回的文本内容（JSON 格式）
     text = response.text
-    # To understand the structure of this text, you may want to save it
-    # to a file and open it in VS Code or a browser.
-    # See the README file for more information.
-    ...
 
-    # We need to interpret the text to get values that we can work with.
-    # What format is the text in? How can we load the values?
-    return ...
+    # 把 JSON 字符串解析成 Python 字典
+    data = json.loads(text)
+
+    # 返回数据
+    return data
+
 
 def count_earthquakes(data):
-    """Get the total number of earthquakes in the response."""
-    return ...
+    """返回总地震数量"""
+    return len(data["features"])   # 所有地震都在 "features" 列表里
 
 
 def get_magnitude(earthquake):
-    """Retrive the magnitude of an earthquake item."""
-    return ...
+    """获取某个地震的震级"""
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
-    """Retrieve the latitude and longitude of an earthquake item."""
-    # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    """获取某个地震的位置（经纬度）"""
+    # "geometry" -> "coordinates" 是一个 [lon, lat, depth] 列表
+    lon, lat, _ = earthquake["geometry"]["coordinates"]
+    return (lat, lon)
 
 
 def get_maximum(data):
-    """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    """找到最大震级及其位置"""
+    max_mag = None
+    max_loc = None
+
+    for eq in data["features"]:
+        mag = get_magnitude(eq)
+        if mag is None:
+            continue  # 有些记录没有震级
+        if (max_mag is None) or (mag > max_mag):
+            max_mag = mag
+            max_loc = get_location(eq)
+
+    return max_mag, max_loc
 
 
-# With all the above functions defined, we can now call them and get the result
-data = get_data()
-print(f"Loaded {count_earthquakes(data)}")
-max_magnitude, max_location = get_maximum(data)
-print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
+# 主程序入口
+if __name__ == "__main__":
+    data = get_data()
+    print(f"Loaded {count_earthquakes(data)} earthquakes")
+
+    max_magnitude, max_location = get_maximum(data)
+    print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
